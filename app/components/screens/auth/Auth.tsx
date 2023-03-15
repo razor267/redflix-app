e611050834a0b9e3c08e48547dc9e84d073be958
Petr Baiborin <razor267@mail.ru>
2023-03-15 17:30:38 +0300

    06. add auth fields

--- app/components/screens/auth/Auth.tsx
+++ app/components/screens/auth/Auth.tsx
@@ -2,7 +2,9 @@ import React, { FC, useState } from 'react'
 import { SubmitHandler, useForm } from 'react-hook-form'
 import { Pressable, Text, View } from 'react-native'
 
+import AuthFields from '@/components/screens/auth/AuthFields'
 import { Button, Loader } from '@/components/ui'
+import DismissKeyboard from '@/components/ui/form-elements/field/DismissKeyboard'
 
 import { IAuthFormData } from '@/shared/types/auth.interface'
 
@@ -13,32 +15,37 @@ const Auth: FC = () => {
 		mode: 'onChange'
 	})
 
-	const onSubmit: SubmitHandler<IAuthFormData> = ({ email, password }) => {}
+	const onSubmit: SubmitHandler<IAuthFormData> = ({ email, password }) => {
+		console.log(email, password)
+	}
 
 	const isLoading = false
 
 	return (
-		<View className='mx-2 items-center justify-center h-full'>
-			<View className='w-9/12'>
-				<Text className='text-center text-white text-4xl font-bold mb-2.5'>
-					{isReg ? 'Register' : 'Login'}
-				</Text>
-				{isLoading ? (
-					<Loader />
-				) : (
-					<>
-						<Button icon={'film'} onPress={handleSubmit(onSubmit)}>
-							Go to watch
-						</Button>
-						<Pressable onPress={() => setIsReg(!isReg)}>
-							<Text className='text-white opacity-30 text-right text-base mt-3'>
-								{isReg ? 'Login' : 'Register'}
-							</Text>
-						</Pressable>
-					</>
-				)}
+		<DismissKeyboard>
+			<View className='mx-2 items-center justify-center h-full'>
+				<View className='w-9/12'>
+					<Text className='text-center text-white text-4xl font-bold mb-2.5'>
+						{isReg ? 'Register' : 'Login'}
+					</Text>
+					{isLoading ? (
+						<Loader />
+					) : (
+						<>
+							<AuthFields control={control} isPassRequired />
+							<Button icon={'film'} onPress={handleSubmit(onSubmit)}>
+								Go to watch
+							</Button>
+							<Pressable onPress={() => setIsReg(!isReg)}>
+								<Text className='text-white opacity-30 text-right text-base mt-3'>
+									{isReg ? 'Login' : 'Register'}
+								</Text>
+							</Pressable>
+						</>
+					)}
+				</View>
 			</View>
-		</View>
+		</DismissKeyboard>
 	)
 }
 
