e213ea6eab851d07d88245254fc0cba3c7188b51
Petr Baiborin <razor267@mail.ru>
2023-03-15 16:21:36 +0300

    05. add react-hook-form, add Button component

--- app/components/screens/auth/Auth.tsx
+++ app/components/screens/auth/Auth.tsx
@@ -1,10 +1,43 @@
-import React, { FC } from 'react'
-import { Text, View } from 'react-native'
+import React, { FC, useState } from 'react'
+import { SubmitHandler, useForm } from 'react-hook-form'
+import { Pressable, Text, View } from 'react-native'
+
+import { Button, Loader } from '@/components/ui'
+
+import { IAuthFormData } from '@/shared/types/auth.interface'
 
 const Auth: FC = () => {
+	const [isReg, setIsReg] = useState(false)
+
+	const { handleSubmit, reset, control } = useForm<IAuthFormData>({
+		mode: 'onChange'
+	})
+
+	const onSubmit: SubmitHandler<IAuthFormData> = ({ email, password }) => {}
+
+	const isLoading = false
+
 	return (
-		<View>
-			<Text>Auth</Text>
+		<View className='mx-2 items-center justify-center h-full'>
+			<View className='w-9/12'>
+				<Text className='text-center text-white text-4xl font-bold mb-2.5'>
+					{isReg ? 'Register' : 'Login'}
+				</Text>
+				{isLoading ? (
+					<Loader />
+				) : (
+					<>
+						<Button icon={'film'} onPress={handleSubmit(onSubmit)}>
+							Go to watch
+						</Button>
+						<Pressable onPress={() => setIsReg(!isReg)}>
+							<Text className='text-white opacity-30 text-right text-base mt-3'>
+								{isReg ? 'Login' : 'Register'}
+							</Text>
+						</Pressable>
+					</>
+				)}
+			</View>
 		</View>
 	)
 }
--- app/navigation/user.routes.ts
+++ app/navigation/user.routes.ts
@@ -1,13 +1,12 @@
 import Auth from '@/components/screens/auth/Auth'
-import Home from '@/components/screens/home/Home'
 
 import { IRoute } from './navigation.types'
 
 export const userRoutes: IRoute[] = [
-	{
+	/*{
 		name: 'Home',
 		component: Home
-	},
+	},*/
 	{
 		name: 'Auth',
 		component: Auth
